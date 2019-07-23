49168443ff45fb0c8bd3af6297662f8eb6518ced
alsandwls1 <alsandwls1@gmail.com>
2019-07-23 23:54:18 +0900

    use v-for

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,11 +1,24 @@
 <template>
     <div class="signin">
-        <h2>회원가입</h2>
+        <div>
+            <h4>참고문서</h4>
+            <p>https://firebase.google.com/docs/firestore/quickstart?hl=ko</p>
+        </div>
 
         <div>
-            ID: <input type="text" v-model="userId">
-            PW: <input type="text" v-model="userPw"> 
-            <button v-on:click="sendUserData">회원가입</button>
+            <h2>회원가입</h2>
+            <span>NAME: </span><input type="text" v-model="user.name">
+            <span>EMAIL: </span><input type="text" v-model="user.email"> 
+            <button v-on:click="addUserData">등록</button>
+        </div>
+
+        <div>
+            <h2>회원목록</h2>
+            <button v-on:click="getUserList">조회</button>
+            <ul>
+                <li v-for="(u, index) in userList" v-bind:key="index">{{ u.name }}: {{ u.email }}</li>
+            </ul>
+
         </div>
     </div>
 </template>
@@ -16,7 +29,7 @@
     text-align: center;
 }
 .signin > div {
-    margin-top: 20px;
+    margin-bottom: 50px;
 }
 button {
     margin: 0px 5px;
@@ -26,21 +39,53 @@ button {
 
 <script>
 /* eslint-disable no-console */ 
+import firebase from 'firebase'
 
-
+var db = firebase.firestore();
 var data = {
-    'userId' : '',
-    'userPw' : ''
+    user: {
+        'name': '',
+        'email': ''
+    },
+    userList: [],
 }
+
 export default {
     name: 'SignIn',
     data () {
         return data;
     },
     methods: {
-        sendUserData () {
-            console.log(this);
-            console.log(this.userId, this.userPw);
+        // 사용자정보 등록
+        addUserData () {
+            var user = this.user;
+
+            db.collection('users').add(this.user)
+            .then(() => {
+                user.name = '';
+                user.email = '';
+            })
+            .catch((error) => {
+                console.log("error=>", error);
+            })
+        },
+        // 사용자 목록 조회
+        getUserList () {
+            let userList = [];
+
+            db.collection('users').get()
+            .then((rtnObj) => {
+                if(rtnObj.empty) {
+                    console.log("데이터 없음.");
+                    return;
+                }
+                rtnObj.forEach(doc => {
+                    console.log(doc.id, '=>', doc.data())
+                    userList.push(doc.data())
+                });
+            })
+            
+            this.userList = userList;
         }
     }
 }
